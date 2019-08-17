b90581d46d19f8850400be1da2fb30216e9ab9b1
Zheng SHAO <z-shao@colopl.co.jp>
2019-08-17 12:41:58 +0900

    try to reproduce segv issue

--- php/index.php
+++ php/index.php
@@ -1,13 +1,24 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/include.php';
 
-use Google\Cloud\Spanner\Database;
+$rc = new RedisCluster(NULL, Array("redis-cluster:7000"), 1.5, 1.5);
+$ra = new RedisArray(array("redis0", "redis1", "redis2"));
 
-startTrace();
+$length = 1000;
 
-spannerDbContext(function(Database $db) {
-    $start = microtime(true);
-    $row = $db->execute('SELECT "Hello, world"')->rows()->current();
-    printf('Query time: %.2f ms'.PHP_EOL, (microtime(true) - $start) * 1000);
-});
+for($i = 0; $i < 10; ++$i) {
+    $ra->setOption(Redis::OPT_PREFIX, 'response-cache:');
+    $ra->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_MSGPACK);
+    $rc->setOption(Redis::OPT_PREFIX, 'response-cache:');
+    $rc->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_MSGPACK);
+
+    $ra->get("user1:name");
+    $ra->setex("user1:name", 900, substr(bin2hex(random_bytes($length)), 0, $length));
+    $rc->get("user1:name");
+    $rc->setex("user1:name", 900, substr(bin2hex(random_bytes($length)), 0, $length));
+
+    $ra->setOption(Redis::OPT_PREFIX, 'test:');
+    $ra->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_IGBINARY);
+    $rc->setOption(Redis::OPT_PREFIX, 'test:');
+    $rc->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_IGBINARY);
+}
